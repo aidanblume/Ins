a714c8be9adb6c335b5647aee10ff26955178923
nblume <nblume@lacare.org>
2018-02-02 06:44:22 -0800

    adding progress on ADT db exploration

--- Code/Data_Acquisition_and_Understanding/ENCP__Encounter__Admit_Dischrg_transf_data_snc/field_by_field_exploration.sql
+++ Code/Data_Acquisition_and_Understanding/ENCP__Encounter__Admit_Dischrg_transf_data_snc/field_by_field_exploration.sql
@@ -79,3 +79,159 @@ AND DS_VISIT_STATUS_ID = 75
 GROUP BY patient_type_id
 ;
 
+--WHAT IS THE COUNT BY PATIENT TYPE? 
+--Also well represented across levels. 
+--How are the vars "Patient_TYpe " and "Patient_Class" related to each other? Is one more granular?
+SELECT patient_type_id
+, CASE
+    WHEN patient_type_id=108 THEN 'community programs'
+    WHEN patient_type_id=25 THEN 'emergency room'
+    WHEN patient_type_id=106 THEN 'home health service'
+    WHEN patient_type_id=28 THEN 'inpatient'
+    WHEN patient_type_id=135 THEN 'lab'
+    WHEN patient_type_id=104 THEN 'med/surg'
+    WHEN patient_type_id=101 THEN 'newborn'
+    WHEN patient_type_id=139 THEN 'newborn-dvt mapped'
+    WHEN patient_type_id=107 THEN 'ob/gyn'
+    WHEN patient_type_id=26 THEN 'observation'
+    WHEN patient_type_id=102 THEN 'other'
+    WHEN patient_type_id=27 THEN 'outpatient'
+    WHEN patient_type_id=103 THEN 'pediatrics'
+    WHEN patient_type_id=134 THEN 'pre-admit'
+    WHEN patient_type_id=91 THEN 'psychiatry'
+    WHEN patient_type_id=105 THEN 'rehab/snf'
+    WHEN patient_type_id=150 THEN 'no xref'
+    WHEN patient_type_id=149 THEN 'unset'
+    END AS patient_type_description
+, COUNT(patient_type_id) AS Frequency
+FROM ENCOUNTER.ADMIT_DISCHRG_TRANSF_DATA_SNC
+WHERE ds_visit_type_id = 70 --inpatient visits only. So result here should only be inpatient class. 
+AND DS_VISIT_STATUS_ID = 75 
+GROUP BY patient_type_id
+;
+
+--ADMIT_SOURCE_ID and _TYPE: i.e. where was the member before their admit
+SELECT admit_source_id
+, CASE
+    WHEN admit_source_id=22 THEN 'affiliated hospital referral'
+    WHEN admit_source_id=87 THEN 'affiliated hospital transfer'
+    WHEN admit_source_id=14 THEN 'affiliated outpatient referral'
+    WHEN admit_source_id=20 THEN 'clinic specialty referral'
+    WHEN admit_source_id=17 THEN 'emergency'
+    WHEN admit_source_id=69 THEN 'home health service transfer'
+    WHEN admit_source_id=19 THEN 'law'
+    WHEN admit_source_id=21 THEN 'managed care referral'
+    WHEN admit_source_id=121 THEN 'newborn'
+    WHEN admit_source_id=68 THEN 'non affiliated hospital'
+    WHEN admit_source_id=13 THEN 'non affiliated hospital referral'
+    WHEN admit_source_id=67 THEN 'non affiliated hospital transfer'
+    WHEN admit_source_id=88 THEN 'non affiliated hospital transfer'
+    WHEN admit_source_id=23 THEN 'non affiliated outpatient referral'
+    WHEN admit_source_id=16 THEN 'other'
+    WHEN admit_source_id=122 THEN 'outpatient'
+    WHEN admit_source_id=63 THEN 'outpatient transfer'
+    WHEN admit_source_id=24 THEN 'physician referral'
+    WHEN admit_source_id=64 THEN 'rehab/snf'
+    WHEN admit_source_id=18 THEN 'rehab/snf referral'
+    WHEN admit_source_id=15 THEN 'rehab/snf referral'
+    WHEN admit_source_id=66 THEN 'rehab/snf transfer'
+    WHEN admit_source_id=89 THEN 'self referral'
+    WHEN admit_source_id=146 THEN 'No Xref'
+    WHEN admit_source_id=145 THEN 'unset'
+    END AS admit_source_description
+, COUNT(admit_source_id) AS Frequency
+FROM ENCOUNTER.ADMIT_DISCHRG_TRANSF_DATA_SNC
+WHERE ds_visit_type_id = 70 --inpatient visits only. So result here should only be inpatient class. 
+AND DS_VISIT_STATUS_ID = 75 
+GROUP BY admit_source_id
+;
+
+--ADMIT_SOURCE_ID and _TYPE: i.e. where was the member before their admit
+SELECT admit_source_id
+, CASE
+    WHEN admit_source_id=22 THEN 'affiliated hospital referral'
+    WHEN admit_source_id=87 THEN 'affiliated hospital transfer'
+    WHEN admit_source_id=14 THEN 'affiliated outpatient referral'
+    WHEN admit_source_id=20 THEN 'clinic specialty referral'
+    WHEN admit_source_id=17 THEN 'emergency'
+    WHEN admit_source_id=69 THEN 'home health service transfer'
+    WHEN admit_source_id=19 THEN 'law'
+    WHEN admit_source_id=21 THEN 'managed care referral'
+    WHEN admit_source_id=121 THEN 'newborn'
+    WHEN admit_source_id=68 THEN 'non affiliated hospital'
+    WHEN admit_source_id=13 THEN 'non affiliated hospital referral'
+    WHEN admit_source_id=67 THEN 'non affiliated hospital transfer'
+    WHEN admit_source_id=88 THEN 'non affiliated hospital transfer'
+    WHEN admit_source_id=23 THEN 'non affiliated outpatient referral'
+    WHEN admit_source_id=16 THEN 'other'
+    WHEN admit_source_id=122 THEN 'outpatient'
+    WHEN admit_source_id=63 THEN 'outpatient transfer'
+    WHEN admit_source_id=24 THEN 'physician referral'
+    WHEN admit_source_id=64 THEN 'rehab/snf'
+    WHEN admit_source_id=18 THEN 'rehab/snf referral'
+    WHEN admit_source_id=15 THEN 'rehab/snf referral'
+    WHEN admit_source_id=66 THEN 'rehab/snf transfer'
+    WHEN admit_source_id=89 THEN 'self referral'
+    WHEN admit_source_id=146 THEN 'No Xref'
+    WHEN admit_source_id=145 THEN 'unset'
+    END AS admit_source_description
+, COUNT(admit_source_id) AS Frequency
+FROM ENCOUNTER.ADMIT_DISCHRG_TRANSF_DATA_SNC
+WHERE ds_visit_type_id = 70 --inpatient visits only. So result here should only be inpatient class. 
+AND DS_VISIT_STATUS_ID = 75 
+GROUP BY admit_source_id
+;
+
+--what is the relationship between this field and others identifying visit type?
+SELECT admit_type_id
+, CASE
+    WHEN admit_type_id=9 THEN 'emergency room'
+    WHEN admit_type_id=137 THEN 'inpatient'
+    WHEN admit_type_id=138 THEN 'lab'
+    WHEN admit_type_id=7 THEN 'newborn'
+    WHEN admit_type_id=11 THEN 'other'
+    WHEN admit_type_id=10 THEN 'outpatient'
+    WHEN admit_type_id=12 THEN 'rehab/snf'
+    WHEN admit_type_id=90 THEN 'self referral'
+    WHEN admit_type_id=8 THEN 'urgent care'
+    WHEN admit_type_id=154 THEN 'No Xref'
+    WHEN admit_type_id=153 THEN 'Unset'
+    END AS admit_type_description
+, COUNT(admit_type_id) AS Frequency
+FROM ENCOUNTER.ADMIT_DISCHRG_TRANSF_DATA_SNC
+WHERE ds_visit_type_id = 70 --inpatient visits only. So result here should only be inpatient class. 
+AND DS_VISIT_STATUS_ID = 75 
+GROUP BY admit_type_id
+;
+
+--ADMIT_DATE: Frequency by Year
+SELECT 
+EXTRACT(YEAR FROM ADMIT_DATE) AS "Admit_Year"
+, COUNT(EXTRACT(YEAR FROM ADMIT_DATE)) AS "Frequency"
+FROM ENCOUNTER.ADMIT_DISCHRG_TRANSF_DATA_SNC
+WHERE ds_visit_type_id = 70 --INPATIENT
+GROUP BY EXTRACT(YEAR FROM ADMIT_DATE)
+ORDER BY  EXTRACT(YEAR FROM ADMIT_DATE) DESC
+;
+
+--HOW MUCH DATA YESTERDAY? 4 DAYS AGO? 30 DAYS AGO? HOW MUCH "IN FUTURE"/error?
+SELECT COUNT(*)
+FROM ENCOUNTER.ADMIT_DISCHRG_TRANSF_DATA_SNC
+WHERE ds_visit_type_id = 70 --INPATIENT
+AND ADMIT_DATE IS NOT NULL
+AND ADMIT_DATE BETWEEN '29-JAN-18' AND '30-JAN-18'
+ORDER BY ADMIT_DATE DESC
+;
+--226
+--30 dates for today (possibly in error)
+--50 dates in the future (clear error)
+
+
+--RECENCY OF ADMIT DATA (run on 2/1)
+SELECT *
+FROM ENCOUNTER.ADMIT_DISCHRG_TRANSF_DATA_SNC
+WHERE ds_visit_type_id = 70 --INPATIENT
+AND ADMIT_DATE IS NOT NULL
+--AND ADMIT_DATE BETWEEN '31-JAN-18' AND '01-FEB-18'
+ORDER BY ADMIT_DATE DESC
+;
