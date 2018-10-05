6f50e77f2c11ddc5a891a5790a00c8054382e783
Nathalie Blume <nblume@lacare.org>
2018-10-05 15:46:21 -0700

    Revert "Foundational code review"

--- Code/Data_Acquisition_and_Understanding/Cloudera DSW/Iteration 2/step06_demog.sql
+++ Code/Data_Acquisition_and_Understanding/Cloudera DSW/Iteration 2/step06_demog.sql
@@ -12,123 +12,144 @@ drop table if exists nathalie.prjrea_step6_demog;
 create table nathalie.prjrea_step6_demog
 as
 select 
-    Ca.*
-    , S.gender
-    , S.language_written_code
-    , S.ethnicity_code
-    , S.zip_code
-    , S.zip4
-    , S.phone
-    , S.has_phone
-    , S.deathdate
-    , S.dies_before_discharge
-    , S.is_a_30d_death
-    , S.dob
-    , S.adm_age 
-    , S.First_of_adm_mth_age 
+    *
     , case
-            when S.adm_age is null then null
-            when S.adm_age <= 17 then 'C' --child
-            when S.adm_age > 17 and S.adm_age < 65 then 'A' --adult
-            when S.adm_age >= 65 and S.adm_age <= 150 then 'O' --older adult
+            when adm_age is null then null
+            when adm_age <= 17 then 'C' --child
+            when adm_age > 17 and adm_age < 65 then 'A' --adult
+            when adm_age >= 65 and adm_age <= 150 then 'O' --older adult
             else null
         end as agegp_hedis 
     , case
-            when S.adm_age is null then null
-            when S.adm_age < 16 then 'C' --child
-            when S.adm_age >= 16 and S.adm_age < 26 then 'T' --transition age adult, aka TAY
-            when S.adm_age >= 26 and S.adm_age < 60 then 'A' --adult
-            when S.adm_age >= 60 and S.adm_age <= 150 then 'O' --older adult
+            when adm_age is null then null
+            when adm_age < 16 then 'C' --child
+            when adm_age >= 16 and adm_age < 26 then 'T' --transition age adult, aka TAY
+            when adm_age >= 26 and adm_age < 60 then 'A' --adult
+            when adm_age >= 60 and adm_age <= 150 then 'O' --older adult
             else null
         end as agegp_cty 
     , case
-            when S.First_of_adm_mth_age is null then null
-            when S.First_of_adm_mth_age < 19 then 'C' --child
-            when S.First_of_adm_mth_age >= 19 and S.First_of_adm_mth_age < 65 then 'A' --adult
-            when S.First_of_adm_mth_age >= 65 and S.First_of_adm_mth_age <= 150 then 'O' --older adult
+            when 1st_of_adm_mth_age is null then null
+            when 1st_of_adm_mth_age < 19 then 'C' --child
+            when 1st_of_adm_mth_age >= 19 and 1st_of_adm_mth_age < 65 then 'A' --adult
+            when 1st_of_adm_mth_age >= 65 and 1st_of_adm_mth_age <= 150 then 'O' --older adult
             else null
         end as agegp_lob_rollup 
-from nathalie.prjrea_step5_PPG_LOB_PCP as Ca
-left join
+from
 (
-
-    select  
-        C.*
-        , case
-                when C.phone is null then 0
-                else 1
-            end as has_phone
-        , case 
-            when datediff(C.deathdate, C.dis_dt) <= 0 then 1
-                else 0
-            end as dies_before_discharge
-        , case
-                when C.deathdate <= adddate(C.dis_dt, 30) then 1
-                else 0
-            end as is_a_30d_death
-        , case
-                when C.dob is null then null
-                else floor(datediff(C.adm_dt, C.dob) / 365.25) 
-            end as adm_age 
-        , case
-                when C.dob is null then null
-                else floor(datediff(trunc(C.adm_dt, 'month'), C.dob) / 365.25) 
-            end as First_of_adm_mth_age 
-        , row_number() over(partition by C.case_id order by C.priority asc, C.zip_code, C.zip4, C.phone asc) as rn --ordering in part is meant to avoid nulls
+    select *
     from 
     (
 
-        --look for cin_no in cin_no field of encp.members
-        select
-            A.case_id
-            , A.adm_dt
-            , A.dis_dt
-            , B2.*
-            , 1 as priority
-        from nathalie.prjrea_step5_PPG_LOB_PCP as A 
-        left join 
+        select C_inner.*, row_number() over(partition by C_inner.case_id order by C_inner.priority asc, C_inner.zip_code, C_inner.zip4, C_inner.has_phone desc) as rn --ordering in part is meant to avoid nulls
+        from 
         (
-            select *
-            from
-            (
-                select cin_no, gender, language_written_code, ethnicity_code, zip_code, zip4, phone, deathdate, dob
-                    , row_number() over(partition by cin_no order by dob, gender, zip_code, zip4, language_written_code, ethnicity_code, phone, deathdate) as rownumner1652
-                from encp.members 
-            ) as B1
-            where rownumner1652 = 1 and cin_no is not null
-        ) B2
-        on A.cin_no = B2.cin_no
-        where not (A.cin_no like '%*ZZ%' or A.cin_no like '%*01%') -- remove non-standard cin_nos. 
-
-        union
-        
-        --look for cin_no in mhc_member_no field
-        select
-            A.case_id
-            , A.adm_dt
-            , A.dis_dt
-            , B2.*
-            , 2 as priority
-        from nathalie.prjrea_step5_PPG_LOB_PCP as A 
-        left join 
-        (
-            select *
-            from
-            (
-                select mhc_member_no, gender, language_written_code, ethnicity_code, zip_code, zip4, phone, deathdate, dob
-                    , row_number() over(partition by mhc_member_no order by dob, gender, zip_code, zip4, language_written_code, ethnicity_code, phone, deathdate) as rownumner1652
-                from encp.members 
-            ) as B1
-            where rownumner1652 = 1 and mhc_member_no is not null
-        ) B2
-        on A.cin_no = B2.mhc_member_no
-        where A.cin_no like '%*01%'
-        
-    ) C
     
+            --look for cin_no in cin_no field of encp.members
+            select
+                A.*
+                , B2.gender
+                , language_written_code
+                , ethnicity_code
+                , zip_code
+                , zip4
+                , phone
+                , case
+                        when phone is null then 0
+                        else 1
+                    end as has_phone
+                , B2.deathdate
+                , case 
+                    when datediff(deathdate, dis_dt) <= 0 then 1
+                        else 0
+                    end as dies_before_discharge
+                , case
+                        when deathdate <= adddate(dis_dt, 30) then 1
+                        else 0
+                    end as is_a_30d_death
+                , B2.dob
+                , case
+                        when dob is null then null
+                        else floor(datediff(A.adm_dt, B2.dob) / 365.25) 
+                    end as adm_age 
+                , case
+                        when dob is null then null
+                        else floor(datediff(trunc(A.adm_dt, 'month'), B2.dob) / 365.25) 
+                    end as 1st_of_adm_mth_age 
+                , 1 as priority
+            from nathalie.prjrea_step5_PPG_LOB_PCP as A 
+            left join 
+            (
+                select *
+                from
+                (
+                    select cin_no, gender, language_written_code, ethnicity_code, zip_code, zip4, phone, deathdate, dob
+                        , row_number() over(partition by cin_no order by dob, gender, zip_code, zip4, language_written_code, ethnicity_code, phone, deathdate) as rownumner1652
+                    from encp.members 
+                ) as B1
+                where rownumner1652 = 1 and cin_no is not null
+            ) B2
+            on A.cin_no = B2.cin_no
+            where not (A.cin_no like '%*ZZ%' or A.cin_no like '%*01%') -- remove non-standard cin_nos. 
+
+            union
+            
+            --look for cin_no in mhc_member_no field
+            select
+                A.*
+                , B2.gender
+                , language_written_code
+                , ethnicity_code
+                , zip_code
+                , zip4
+                , phone
+                , case
+                        when phone is null then 0
+                        else 1
+                    end as has_phone
+                , B2.deathdate
+                , case 
+                    when datediff(deathdate, dis_dt) <= 0 then 1
+                        else 0
+                    end as dies_before_discharge
+                , case
+                        when deathdate <= adddate(dis_dt, 30) then 1
+                        else 0
+                    end as is_a_30d_death
+                , B2.dob
+                , case
+                        when dob is null then null
+                        else floor(datediff(A.adm_dt, B2.dob) / 365.25) 
+                    end as adm_age 
+                , case
+                        when dob is null then null
+                        else floor(datediff(trunc(A.adm_dt, 'month'), B2.dob) / 365.25) 
+                    end as 1st_of_adm_mth_age 
+                , 2 as priority
+            from nathalie.prjrea_step5_PPG_LOB_PCP as A 
+            left join 
+            (
+                select *
+                from
+                (
+                    select mhc_member_no, gender, language_written_code, ethnicity_code, zip_code, zip4, phone, deathdate, dob
+                        , row_number() over(partition by mhc_member_no order by dob, gender, zip_code, zip4, language_written_code, ethnicity_code, phone, deathdate) as rownumner1652
+                    from encp.members 
+                ) as B1
+                where rownumner1652 = 1 and mhc_member_no is not null
+            ) B2
+            on A.cin_no = B2.mhc_member_no
+            where A.cin_no like '%*01%' or A.cin_no is null -- there are indeed a few nulls even with the union statement. Is there a better way to retrieve member info?
+        ) C_inner
+    ) C
+    where rn = 1
+        
 ) as S
-on Ca.case_id=S.case_id
-where S.rn = 1 or S.rn is null -- there are a few cin_nos with null demog data. Is there another table we can use?
 --where dis_status not in ('20', '40', '41', '42') --member did not expire before discharge
 --and datediff(deathdate, dis_dt) >= 0 --member did not expire before discharge 
 ;
+
+select count(*) from nathalie.prjrea_step5_PPG_LOB_PCP
+select count(distinct case_id) from nathalie.prjrea_step5_PPG_LOB_PCP --789882
+
+select count(distinct case_id) from nathalie.prjrea_step6_demog; --789835
