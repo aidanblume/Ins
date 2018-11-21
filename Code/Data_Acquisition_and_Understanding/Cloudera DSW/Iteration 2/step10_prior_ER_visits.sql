3ba74f69fb6b6aa10d0ddecf9718853498ff414d
Nathalie Blume <nblume@lacare.org>
2018-11-20 16:34:18 -0800

    Update step10_prior_ER_visits.sql
    Removed qnxt records from hoap enc, clm; extended definition of an ER record to bill types 13, 14 and location (aka pos_1) 23; removed to_date truncation on service date.

--- Code/Data_Acquisition_and_Understanding/Cloudera DSW/Iteration 2/step10_prior_ER_visits.sql
+++ Code/Data_Acquisition_and_Understanding/Cloudera DSW/Iteration 2/step10_prior_ER_visits.sql
@@ -22,7 +22,7 @@ nathalie.prjrea_step9_postdischargeSNF a
 left join
 (
     select a.cin_no, a.adm_dt
-        , sum(case  when (datediff(a.adm_dt, er.er_adm_dt) <= 183 and datediff(a.adm_dt, er.er_adm_dt) >= 1)
+        , sum(case  when (int_months_between(a.adm_dt, er.er_adm_dt) <= 6 and datediff(a.adm_dt, er.er_adm_dt) >= 0)
                     then 1 else 0 end) as count_prior6m_er
     from nathalie.prjrea_step9_postdischargeSNF a
     left join 
@@ -34,16 +34,18 @@ left join
                 , row_number() over(partition by cin_no, case_id, er_adm_dt order by source_table asc) as rownumber
             from
             (  -- union reports of ER events from 4 sources: claims_universe, & HOAP clm, enc, qnxt tables 
-                select distinct claimid as case_id, carriermemid as cin_no, to_date(startdate) as er_adm_dt
+                select distinct claimid as case_id, carriermemid as cin_no, startdate as er_adm_dt
                     , 1 as source_table
                 from SWAT.claims_universe
-                where er_ind = 'yes'
+                where er_ind not in ('non_ER') -- field value has changed.  / avoidable ED algo. 
                 
                 union
-                select distinct case_id, cin_no, to_date(trunc(admit_dt_clm, 'DD')) as er_adm_dt 
-                    , 2 as source_table
-                from HOAP.qnxt_hdr_inpsnf  
-                where substr(type_bill,1,2) in ('11','12')
+            
+                select distinct hdr.case_id, hdr.cin_no, admit_dt_clm as er_adm_dt --note that admit_dt_clm has uninformative hours: they're always 7 or 8 am
+                    , 3 as source_table
+                from hoap.clm_hdr_inpsnf hdr join hoap.clm_detail_inpsnf det 
+                on hdr.cin_no=det.cin_no and hdr.cl_id=det.cl_id
+                where substr(hdr.type_bill,1,2) in ('11','12', '13', '14') 
                 -- Type of Bill Codes (Form Locator 4)
                 -- REF: http://dhs.pa.gov/cs/groups/webcontent/documents/manual/s_001939.pdf
                 -- INPATIENT ONLY:
@@ -69,34 +71,21 @@ left join
                         -- 1 Admit through Discharge Claim
                         -- 7 Replacement of Prior Claim
                         -- 8 Void/Cancel of Prior Claim 
-                and adm_type in ('1','2', '5') 
-                -- Admission Type (Form Locator 14) 
-                -- admission type on UB92, indicates the priority of the inpatient admission. 
-                -- REF: http://dhs.pa.gov/cs/groups/webcontent/documents/manual/s_001939.pdf
-                -- 1 Emergency Admission
-                -- 2 Urgent Admission
-                -- 3 Elective Admission
-                -- 4 Newborn Admission
-                -- 5 Trauma Admission (Emergency Admission)
-                and admit_dt_clm is not null
-                union
-                select distinct hdr.case_id, hdr.cin_no, to_date(trunc(hdr.admit_dt_clm, 'DD')) as er_adm_dt 
-                    , 3 as source_table
-                from hoap.clm_hdr_inpsnf hdr join hoap.clm_detail_inpsnf det 
-                on hdr.cin_no=det.cin_no and hdr.cl_id=det.cl_id
-                where substr(type_bill,1,2) in ('11','12')
-                and det.rev_cd in ('0450', '0451', '0452', '0453', '0454', '0455', '0456', '0457'
-                    , '0458', '0459', '450', '451', '452', '453', '454', '455', '456', '457', '458', '459')
+                and (hdr.pos_1 = '23' OR det.rev_cd in ('0450', '0451', '0452', '0453', '0454', '0455', '0456', '0457', '0458', '0459', '450', '451', '452', '453', '454', '455', '456', '457', '458', '459'))
                 and hdr.admit_dt_clm is not null
+                and bp_code<>'TZGQ'
+
                 union
-                select distinct hdr.case_id, hdr.cin_no, to_date(trunc(hdr.admit_dt_clm, 'DD')) as er_adm_dt 
+                
+                select distinct hdr.case_id, hdr.cin_no, hdr.admit_dt_clm as er_adm_dt 
                     , 4 as source_table
                 from hoap.enc_hdr_inpsnf hdr join hoap.enc_detail_inpsnf det 
                 on hdr.cin_no=det.cin_no and hdr.cl_id=det.cl_id
-                where substr(type_bill,1,2) in ('11','12')
-                and det.rev_cd in ('0450', '0451', '0452', '0453', '0454', '0455', '0456', '0457'
-                    , '0458', '0459', '450', '451', '452', '453', '454', '455', '456', '457', '458', '459')
+                where substr(type_bill,1,2) in ('11','12', '13', '14') 
+                and (hdr.pos_1 = '23' OR det.rev_cd in ('0450', '0451', '0452', '0453', '0454', '0455', '0456', '0457', '0458', '0459', '450', '451', '452', '453', '454', '455', '456', '457', '458', '459'))
                 and hdr.admit_dt_clm is not null
+                and bp_code<>'TZGQ'
+
             ) ALL_ER
         ) rownumber_added
         where rownumber = 1
